59d3961f0e3f07b4c7b2a244f88846addc173afb
Er. Devansh Parikh <65414784+Er-DevanshParikh@users.noreply.github.com>
2021-12-12 12:42:56 +0530

    Initial Commit

---
+++ common_value _dic_value.py
@@ -0,0 +1,22 @@
+array1 = {}
+array2 = {}
+n = int(input("Number of elements :"))
+# Create array from first string values
+print("Enter Elements for Array 1:[Key:Value]")
+for i in range(n):
+    b = input("Enter Key:")
+    c = input("Enter Value:")
+    array1.update({b:c})
+# Create array from second string values
+print("Enter Elements for Array 2:[Key:Value]")
+for i in range(n):
+    m = input("Enter Key:")
+    n = input("Enter Value:")
+    array2.update({m:n})
+
+print(array1)
+print(array2)
+# Find common elements
+common = list(set(array1.values()).intersection(set(array2.values())))
+common.sort()
+print(common)
